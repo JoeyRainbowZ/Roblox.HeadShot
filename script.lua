efd3ad6dfed20ada09d5d89ff363a6f607b1ad79
JoeyRainbowZ <79990012+JoeyRainbowZ@users.noreply.github.com>
2022-04-30 13:14:48 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+local Players = game:getService("Players")
+local plr = Players.LocalPlayer
+local userId = plr.UserId
+local thumbType = Enum.ThumbnailType.HeadShot
+local thumbSize Enum.ThumbnailSize.Size420x420
+local content, isReady = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize)
+
+local imageLabel = script.Parent
+imageLabel.Image = content
