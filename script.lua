0de1221efc4ed4a5738a3c142dd4eb5ed02b4990
JoeyRainbowZ <79990012+JoeyRainbowZ@users.noreply.github.com>
2022-05-03 12:22:59 +0200

    🐛 | Patched a Bug (1)

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local Players = game:getService("Players")
 local plr = Players.LocalPlayer
 local userId = plr.UserId
 local thumbType = Enum.ThumbnailType.HeadShot
-local thumbSize Enum.ThumbnailSize.Size420x420
+local thumbSize = Enum.ThumbnailSize.Size420x420
 local content, isReady = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize)
 
 local imageLabel = script.Parent
