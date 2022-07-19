cf25344868a15b688ab9f8a63e92084e839b9cd6
Patched <79990012+JoeyRainbowZ@users.noreply.github.com>
2022-07-19 20:01:23 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,9 @@
-local Players = game:getService("Players")
-local plr = Players.LocalPlayer
-local userId = plr.UserId
-local thumbType = Enum.ThumbnailType.HeadShot
-local thumbSize = Enum.ThumbnailSize.Size420x420
-local content, isReady = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize)
+local Players = game:getService("Players") -- Get all the Players
+local plr = Players.LocalPlayer -- Get the specific Player
+local userId = plr.UserId -- Get Player's UserID
+local thumbType = Enum.ThumbnailType.HeadShot -- Type of Image (This is the Image from the ROBLOX Home Page)
+local thumbSize = Enum.ThumbnailSize.Size420x420 -- Size (Don't Change!)
+local content, isReady = Players:GetUserThumbnailAsync(userId, thumbType, thumbSize) -- Get all the Data
 
-local imageLabel = script.Parent
-imageLabel.Image = content
+local imageLabel = script.Parent -- The ImageLabel
+imageLabel.Image = content -- The HeadShot from the Player.
